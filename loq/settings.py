5ecca3b9e9005f5958918c1f7ca4fbfa235d7c23
alvinwt <a0073895@nus.edu.sg>
2014-07-24 00:50:32 +0800

    remove sensitive info

--- loq/settings.py
+++ loq/settings.py
@@ -5,7 +5,7 @@ DEBUG = True
 TEMPLATE_DEBUG = False
 
 ADMINS = (
-     ('Alvin', 'a0073895@nus.edu.sg'),
+     ('name','email',)
 )
 
 MANAGERS = ADMINS
@@ -21,7 +21,7 @@ DATABASES = {
         'NAME': 'mysql',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'gtklab',
+        'PASSWORD': 'password',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
